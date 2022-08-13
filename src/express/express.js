fcfcf5a363a955ce7f37b51f7ad61edda6e296b4
Alexander Goncharuk <alexander.goncharuk@brainbase.com>
2022-08-13 13:12:55 +0300

    feat: add error handling to frontend server

--- src/express/express.js
+++ src/express/express.js
@@ -8,13 +8,16 @@ const authRouter = require(`./routes/auth`);
 const searchRouter = require(`./routes/search`);
 const cabinetRouter = require(`./routes/cabinet`);
 const articlesRouter = require(`./routes/articles`);
+const {ErrorMessage, HttpCode} = require('../constants');
+const {getLogger} = require('../service/lib/logger');
 
 const DEFAULT_PORT = 8080;
 const PUBLIC_DIR = `public`;
 const UPLOAD_DIR = `upload`;
-
+const logger = getLogger({name: `front-server`});
 
 const app = express();
+const port = Number.parseInt(process.argv[0], 10) || DEFAULT_PORT;
 
 app.set(`views`, `./src/express/templates`);
 app.set(`view engine`, `pug`);
@@ -28,7 +31,29 @@ app.use(`/search`, searchRouter);
 app.use(`/my`, cabinetRouter);
 app.use(`/articles`, articlesRouter);
 
-app.listen(
-    DEFAULT_PORT,
-    () => console.log(`Server is running on port: ${DEFAULT_PORT}`)
-);
+app.use((req, res) => {
+  const errorMsg = ErrorMessage.routeNotFound(req.url);
+  logger.error(errorMsg);
+  res.status(HttpCode.NOT_FOUND).render(`404`);
+});
+
+app.use((err, _req, res, _next) => {
+  logger.error(`An error occurred on processing request: ${err.message}`);
+  res.status(HttpCode.INTERNAL_SERVER_ERROR).render(`500`);
+});
+
+try {
+  app.listen(
+      port,
+      (err) => {
+        if (err) {
+          return logger.error(`An error occurred on starting the server: ${err.message}`);
+        }
+
+        return logger.info(`Server listening on port: ${port}`);
+      }
+  );
+} catch (e) {
+  logger.error(`An error occurred: ${e.message}`);
+  process.exit(1);
+}
--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -40,7 +40,7 @@ module.exports = {
 
     try {
       app.listen(
-          DEFAULT_PORT,
+        port,
           (err) => {
             if (err) {
               return logger.error(`An error occurred on starting the server: ${err.message}`);
